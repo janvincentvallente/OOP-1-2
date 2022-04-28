23b5ef2e1c2925be5f281e6f3dca23cd6a9ab67f
janvincentvallente <90833880+janvincentvallente@users.noreply.github.com>
2022-04-28 15:09:25 +0800

    Add files via upload

---
+++ Midterm Exam_Program1.py
@@ -0,0 +1,36 @@
+# Program 1
+
+def main():
+    class TemperatureConversion:
+        def __init__(self, temp=1):
+            self._temp = temp
+    class CelsiusToFahrenheit(TemperatureConversion):
+        def conversion(self):
+            return (self._temp * 9) / 5 + 32
+    class CelsiusToKelvin(TemperatureConversion):
+        def conversion(self):
+            return self._temp + 273.15
+    class FahrenheitToCelsius(TemperatureConversion):
+        def conversion(self):
+            return (self._temp - 32) * (5 / 9)
+    class KelvinToCelsius(TemperatureConversion):
+        def conversion(self):
+            return self._temp - 273.15
+
+    tempInCelsius = float(input("Enter the temperature in Celsius: "))
+    convert = CelsiusToKelvin(tempInCelsius)
+    print(str(convert.conversion()) + " Kelvin")
+    convert = CelsiusToFahrenheit(tempInCelsius)
+    print(str(convert.conversion()) + " Fahrenheit")
+
+    tempInFahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+    convert = FahrenheitToCelsius(tempInFahrenheit)
+    print(str(round(convert.conversion(), 2)) + " Celsius")
+
+    tempInKelvin = float(input("Enter the temperature in Kelvin: "))
+    convert = KelvinToCelsius(tempInKelvin)
+    print(str(convert.conversion()) + " Celsius")
+
+
+main()
+
